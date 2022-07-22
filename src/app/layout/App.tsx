04029c34b95f30ac472b8ae5d740424bd8403e45
John Cameron <john.n.cameron@gmail.com>
2022-07-23 08:55:54 +1000

    activity list

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,31 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
-import NavBar from './NavBar';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
-
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get<Activity[]>('https://localhost:5001/Activities').then(response => { 
-      setActivities(response.data);
-    })
+    axios
+      .get<Activity[]>("https://localhost:5001/Activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
 
   return (
     <div className="App">
-      <NavBar />    
-        <Container style={{marginTop: '7em'}}>
-          <List>
-          {activities.map((activity: Activity) => (
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-            ))}
-          </List>
-        </Container>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
     </div>
   );
 }
